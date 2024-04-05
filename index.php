b5109fc55d15545b0e8e61f6a2b56faa6dd56dd1
riyad6392 <109583263+riyad6392@users.noreply.github.com>
2024-04-05 15:26:32 +0600

    Using MVC framework. Update database connection.

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+
+<?php
+require_once 'CrudController.php';
+
+include('crud_connect.php');
+
+$model = new CrudModel(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+$controller = new CrudController($model);
+$result = $controller->getAllEmployees();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +17,7 @@
 	<title>Home Page</title>
 </head>
 <body>
-	<?php include('crud.php');?>
+	
 <h1> CRUD APPLICATION IN PHP</h1>
 
  <a href="create_data.php"> <button>CREATE!</button></a>
@@ -26,8 +38,7 @@
 	</thead>
 	<tbody>
 		<?php 
-         $query = "select * from `employee`";
-         $result = mysqli_query($connection, $query);
+    
 
          if(!$result)
          {
@@ -39,26 +50,22 @@
 
          	while ($row=mysqli_fetch_assoc($result)) {
          		$db_id=$row['id'];
-         		?>
+              ?>
          <tr>
-			<td><?php echo $row['id'];?></td>
-			<td>&ensp;<?php echo $row['name'];?></td>
-			<td>&ensp;<?php echo $row['designation'];?></td>
-			<td>&ensp;<?php echo $row['email'];?></td>
-			<td>&ensp;<?php echo $row['address'];?></td>
-			<td>&ensp;<?php echo $row['salary'];?></td>
-			<td>&ensp;<?php echo $row['status'];?></td>
+			  <td><?php echo $row['id'];?></td>
+			  <td>&ensp;<?php echo $row['name'];?></td>
+			  <td>&ensp;<?php echo $row['designation'];?></td>
+			  <td>&ensp;<?php echo $row['email'];?></td>
+			  <td>&ensp;<?php echo $row['address'];?></td>
+			  <td>&ensp;<?php echo $row['salary'];?></td>
+			  <td>&ensp;<?php echo $row['status'];?></td>
 
 			<td> <a href="single_data.php?edit_id=<?php echo $db_id;?>">Edit</a> || <a href="delete.php?id=<?php echo $db_id ?>">Delete</a></td>
 		</tr>
          		<?php
          	}
 
-         
-
- 
-
-
+        
 
          }
 
