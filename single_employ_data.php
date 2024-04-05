830fe6709f6ac3afd8198d64c465ab7910c1d729
riyad6392 <109583263+riyad6392@users.noreply.github.com>
2024-04-06 02:19:19 +0600

    Update , rename and ignore database connection(already defined)

--- single_employ_data.php
+++ single_employ_data.php
@@ -1,19 +1,18 @@
 <?php
-  require_once 'CrudController.php';
-   include('crud_connect.php');
+  require_once 'MVC_Controller_for_CRUD_application.php'; 
 
  if(isset($_REQUEST['edit_id']))
  {
   $recv_id=$_REQUEST['edit_id'];
-  $model = new CrudModel(HOSTNAME, USERNAME, PASSWORD, DATABASE);
-  $controller = new CrudController($model);
+  $model = new MVC_Model_for_CRUD_application(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+  $controller = new MVC_Controller_for_CRUD_application($model);
   $result = $controller->getAllEmploy($recv_id);
   while($row=mysqli_fetch_assoc($result))
   {
     
 
     ?>
-     <form action="update_data.php" method="post">
+     <form action="update_employ_data.php" method="post">
         <input type="text" name="name" value="<?php  echo $row['name'];?>" placeholder="name">
         <input type="email" name="email" value="<?php  echo $row['email'];?>" placeholder="email">
         <input type="text" name="designation" value="<?php  echo $row['designation'];?>" placeholder="designation">
