7e9b3fa5fed8b41e02530a717d4b72bac5c96460
riyad6392 <109583263+riyad6392@users.noreply.github.com>
2024-04-07 15:08:52 +0600

    used namespace

--- delete_employ_data.php
+++ delete_employ_data.php
@@ -1,5 +1,6 @@
 <?php
-  require_once 'MVC_Controller_for_CRUD_application.php';
+namespace MyApp;
+require 'vendor/autoload.php';
 $recv=$_REQUEST['id'];
 $model = new MVC_Model_for_CRUD_application(HOSTNAME, USERNAME, PASSWORD, DATABASE);
 $controller = new MVC_Controller_for_CRUD_application($model);
